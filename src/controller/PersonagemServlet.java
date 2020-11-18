a46d550890259ca9890d0eae8eb302535f43f7ed
Ingrid <ingrid@MacBook-Air-de-Ingrid.local>
2020-11-18 00:49:55 -0300

    criada servlet

---
+++ src/controller/PersonagemServlet.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.bean.Personagem;
+import model.bo.PersonagemBO;
+
+/**
+ * Servlet implementation class PersonagemServlet
+ */
+@WebServlet("/home")
+public class PersonagemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private PersonagemBO personagemBO = new PersonagemBO();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PersonagemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+//		Personagem personagem =  personagemBO.getPersonagem("");
+		dispatcher.forward(request, response);
+	}
+}
