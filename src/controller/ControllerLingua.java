cf057a2b6c9aac05fd8b4f79969959830ed3c9a9
LucasPelosi21 <lucaspelosi21@gmail.com>
2020-11-18 23:52:48 -0300

    Adicionado internacionalização

---
+++ src/controller/ControllerLingua.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.jstl.core.Config;
+
+/**
+ * Servlet implementation class ControllerLingua
+ */
+@WebServlet("/change")
+public class ControllerLingua extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControllerLingua() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String language = req.getParameter("lingua");
+
+		Locale locale = new Locale(language);
+
+		Config.set(req.getSession(), Config.FMT_LOCALE, locale);
+
+		Config.set(req.getSession(), Config.FMT_FALLBACK_LOCALE, locale);
+
+		resp.sendRedirect("index.jsp?lingua="+locale);
+	}
+
+}
